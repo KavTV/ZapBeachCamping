b3e2ca58bedc49b9bf80ab47b028981586cb9c74
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2021-06-14 14:14:19 +0200

    LIBRARY START

---
+++ Library/ZapLibrary/ZapLibrary/CampingSite.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZapLibrary
+{
+    class CampingSite
+    {
+        public int Id { get => id; }
+        public bool Clean { get => clean;  }
+        public List<string> Typename { get => typename; }
+        internal List<CampingAddition> CampingAdditions { get => campingAdditions; }
+
+        private int id;
+        private bool clean;
+        private List<string> typename;
+        private List<CampingAddition> campingAdditions;
+
+        public CampingSite() { }
+        public CampingSite(int id, bool clean, List<string> typename, List<CampingAddition> campingAdditions)
+        {
+            this.id = id;
+            this.clean = clean;
+            this.typename = typename;
+            this.campingAdditions = campingAdditions;
+        }
+
+        public CampingSite(int id)
+        {
+            this.id = id;
+        }
+    }
+}
---
+++ Library/ZapLibrary/ZapLibrary/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZapLibrary
+{
+    class Customer
+    {
+        public string Email { get => email; }
+        public int Phone { get => phone; }
+        public string Name { get => name; }
+        internal CustomerAddress CustomerAddress { get => cityCode; }
+
+
+        private string email;
+        private int phone;
+        private string name;
+        private CustomerAddress cityCode;
+
+        public Customer(string email)
+        {
+            this.email = email;
+        }
+        public Customer(string email, int phone, string name, CustomerAddress cityCode)
+        {
+            this.email = email;
+            this.phone = phone;
+            this.name = name;
+            this.cityCode = cityCode;
+        }
+    }
+}
