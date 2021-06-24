b01ecffa2c1e56a08244d5c09371468e7ff8ed4c
Kasper Jeppesen <46051618+KavTV@users.noreply.github.com>
2021-06-24 11:14:50 +0200

    end date now must be atleast 1 day after reservation date

--- Website/ZAPWebsite/ZAPWebsite/Scripts/BookingPage.js
+++ Website/ZAPWebsite/ZAPWebsite/Scripts/BookingPage.js
@@ -4,14 +4,18 @@ var resEnd = document.getElementById("MainContent_resEnd");
 
 //Make sure they cant select dates older than today
 //Get todays date
-var tomorrow = new Date();
+var startMin = new Date();
+var endMin = new Date();
 //Add a day, because we dont want people to reserve at the same day
-tomorrow.setDate(tomorrow.getDate() + 1);
+startMin.setDate(startMin.getDate() + 1);
+//End date should not be same day as reservation
+endMin.setDate(endMin.getDate() + 2);
 //Split it into yyyy-mm-dd
-tomorrow = tomorrow.toISOString().split('T')[0];
+startMin = startMin.toISOString().split('T')[0];
+endMin = endMin.toISOString().split('T')[0];
 //Set the minimum date
-resStart.setAttribute('min', tomorrow);
-resEnd.setAttribute('min', tomorrow);
+resStart.setAttribute('min', startMin);
+resEnd.setAttribute('min', endMin);
 
 //If season is selected, then 
 if (seasonCheckbox != null) {
